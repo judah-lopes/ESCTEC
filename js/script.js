448aa834294074bdbfe0a63a45b39f326c4af21b
JudahzinOhBrabo <lopes.judah@gmail.com>
2024-11-08 13:54:42 -0300

    COMMITADA FINALIZANTE MASTER

--- js/script.js
+++ js/script.js
@@ -47,46 +47,51 @@ function preencherFormularioProd(selectElement) {
 
     // Encontrar os campos dentro do mesmo contêiner
     const fValor = produtoItem.querySelector('.valor');
+    const fDesc = produtoItem.querySelector('.descricao');
 
     // Dados dos produtos
     const cadeiras = {
-        cd_01: { valorUnit: 250.00 },
-        cd_02: { valorUnit: 370.00 },
-        cd_03: { valorUnit: 692.00 },
-        cd_04: { valorUnit: 810.00 },
-        cd_05: { valorUnit: 1591.00 },
-        cd_06: { valorUnit: 3180.00 }
+        cd_01: { descricao: "Cadeira Dot - All Black", valorUnit: 250.00 },
+        cd_02: { descricao: "Cadeira Uni - All Black", valorUnit: 370.00 },
+        cd_03: { descricao: "Cadeira Uni Pro - All Black", valorUnit: 692.00 },
+        cd_04: { descricao: "Cadeira My Chair - grafite e azul", valorUnit: 810.00 },
+        cd_05: { descricao: "Cadeira Tecton - night blue", valorUnit: 1591.00 },
+        cd_06: { descricao: "Cadeira Flexform Alpha 2 Pro - preta", valorUnit: 3180.00 }
     };
 
     const mesas = {
-        ms_01: { valorUnit: 663.00 },
-        ms_02: { valorUnit: 239.99 },
-        ms_03: { valorUnit: 1381.00 },
-        ms_04: { valorUnit: 1241.00 },
-        ms_05: { valorUnit: 3715.00 },
-        ms_06: { valorUnit: 251.00 }
+        ms_01: { descricao: "Mesa Dalla Costa em L c/ Gaveteiro", valorUnit: 663.00 },
+        ms_02: { descricao: "Escrivaninha 135cm c/ Gaveteiro Suspenso", valorUnit: 239.99 },
+        ms_03: { descricao: "Mesa Nogal 270cm c/ Caixas de Tomadas", valorUnit: 1381.00 },
+        ms_04: { descricao: "Mesa Nogal 380cm c/ Caixas de Tomadas", valorUnit: 1241.00 },
+        ms_05: { descricao: "Mesa Industrial Redonda 110cm", valorUnit: 3715.00 },
+        ms_06: { descricao: "Mesa L 120x120cm - Tampo 2,5cm", valorUnit: 251.00 }
     };
     
     const diversos = {
-        dv_01: { valorUnit: 181.83 },
-        dv_02: { valorUnit: 679.00 },
-        dv_03: { valorUnit: 802.00 },
-        dv_04: { valorUnit: 599.00 },
-        dv_05: { valorUnit: 829.00 },
-        dv_06: { valorUnit: 699.99 },
-        dv_07: { valorUnit: 10.56 },
-        dv_08: { valorUnit: 164.00 }
+        dv_01: { descricao: "Kit de acessórios My Desk", valorUnit: 181.83 },
+        dv_02: { descricao: "Mesa Jump Baixa", valorUnit: 679.00 },
+        dv_03: { descricao: "Banco Benni", valorUnit: 802.00 },
+        dv_04: { descricao: "Roma Espera", valorUnit: 599.00 },
+        dv_05: { descricao: "Poltrona Vitto", valorUnit: 829.00 },
+        dv_06: { descricao: "Mesa de centro Mince - preta", valorUnit: 699.99 },
+        dv_07: { descricao: "Organizador de Gaveta 17x25,5cm - preto", valorUnit: 10.56 },
+        dv_08: { descricao: "Suporte Multiarticulado de Mesa - grafite", valorUnit: 164.00 }
     };    
 
     // Verifica se o produto selecionado está nas cadeiras, mesas ou diversos
     if (cadeiras[valorProdutoSelecionado]) {
         fValor.value = cadeiras[valorProdutoSelecionado].valorUnit.toFixed(2);
+        fDesc.value = cadeiras[valorProdutoSelecionado].descricao;
     } else if (mesas[valorProdutoSelecionado]) {
         fValor.value = mesas[valorProdutoSelecionado].valorUnit.toFixed(2);
+        fDesc.value = mesas[valorProdutoSelecionado].descricao;
     } else if (diversos[valorProdutoSelecionado]) {
         fValor.value = diversos[valorProdutoSelecionado].valorUnit.toFixed(2);
+        fDesc.value = diversos[valorProdutoSelecionado].descricao;
     } else {
         fValor.value = ""; // Limpa o campo caso nenhum produto seja encontrado
+        fDesc.value = "";
     }
 }
 
@@ -140,46 +145,50 @@ function adicionarProduto() {
 
     // Inserir HTML do novo produto
     novoProduto.innerHTML = `
+        <hr style="width: 70%; margin: 0 auto 25px;">
         <label>Produto ${contadorProdutos}</label><br>
         <select class="produto" onchange="preencherFormularioProd(this)" required>
-                <option value="">Selecione um produto</option>
-                
-                <!-- Cadeiras -->
-                <option value="">---- CADEIRAS --------------</option>
-                <option value="cd_01">Cadeira Dot - All Black</option>
-                <option value="cd_02">Cadeira Uni - All Black</option>
-                <option value="cd_03">Cadeira Uni Pro - All Black</option>
-                <option value="cd_04">Cadeira My Chair - grafite e azul</option>
-                <option value="cd_05">Cadeira Tecton - night blue</option>
-                <option value="cd_06">Cadeira Flexform Alpha 2 Pro - preta</option>
-                
-                <!-- Mesas -->
-                <option value="">---- MESAS -----------------</option>
-                <option value="ms_01">Mesa Dalla Costa em L c/ Gaveteiro</option>
-                <option value="ms_02">Escrivaninha 135cm c/ Gaveteiro Suspenso</option>
-                <option value="ms_03">Mesa Nogal 270cm c/ Caixas de Tomadas</option>
-                <option value="ms_05">Mesa Nogal 380cm c/ Caixas de Tomadas</option>
-                <option value="ms_04">Mesa Industrial Redonda 110cm</option>
-                <option value="ms_06">Mesa L 120x120cm - Tampo 2,5cm</option>
-                
-                <!-- Diversos -->
-                <option value="">---- DIVERSOS -------------</option>
-                <option value="dv_01">Kit de acessórios My Desk</option>
-                <option value="dv_02">Mesa Jump Baixa</option>
-                <option value="dv_03">Banco Benni</option>
-                <option value="dv_04">Roma Espera</option>
-                <option value="dv_05">Poltrona Vitto</option>
-                <option value="dv_06">Mesa de centro Mince - preta</option>
-                <option value="dv_07">Organizador de Gaveta 17x25,5cm - preto</option>
-                <option value="dv_08">Suporte Multiarticulado de Mesa - grafite</option>
-            </select>
+            <option value="">Selecione um produto</option>
             
+            <!-- Cadeiras -->
+            <option value="">---- CADEIRAS --------------</option>
+            <option value="cd_01">CD01</option>
+            <option value="cd_02">CD02</option>
+            <option value="cd_03">CD03</option>
+            <option value="cd_04">CD04</option>
+            <option value="cd_05">CD05</option>
+            <option value="cd_06">CD06</option>
+            
+            <!-- Mesas -->
+            <option value="">---- MESAS -----------------</option>
+            <option value="ms_01">MS01</option>
+            <option value="ms_02">MS02</option>
+            <option value="ms_03">MS03</option>
+            <option value="ms_05">MS04</option>
+            <option value="ms_04">MS05</option>
+            <option value="ms_06">MS06</option>
+            
+            <!-- Diversos -->
+            <option value="">---- DIVERSOS -------------</option>
+            <option value="dv_01">DV01</option>
+            <option value="dv_02">DV02</option>
+            <option value="dv_03">DV03</option>
+            <option value="dv_04">DV04</option>
+            <option value="dv_05">DV05</option>
+            <option value="dv_06">DV06</option>
+            <option value="dv_07">DV07</option>
+            <option value="dv_08">DV08</option>
+        </select>
         
+        <input type="text" class="descricao" placeholder="Descrição" readonly>
+
+        <div class="qtd-valor">
             <input type="number" class="qtd" placeholder="Quantidade" min="1" onchange="calcValorTotalProduto(this)" required>
-            <input type="text" class="valor" placeholder="Valor Unitário" required>
+            <input type="text" class="valor" placeholder="Valor Unitário" required readonly>
+        </div>
         
-            <label>Total: 
-            <input type="text" class="total" placeholder="Valor Total" readonly>
+        <label>Total: 
+        <input type="text" class="total" placeholder="Valor Total" readonly>
     `;
 
     contadorProdutos++;
